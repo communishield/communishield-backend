1f39319b7892ef3dcc223078a053c32dcb4d2f7d
André (ugsto) Bortoli <andre.ugsto@proton.me>
2023-11-15 20:02:47 -0300

    refactor: abstract away boilerplate into abstract class

--- src/api/routes/auth.ts
+++ src/api/routes/auth.ts
@@ -1,25 +1,9 @@
 import KoaRouter from "@koa/router";
-import { type Router } from "../interfaces/router";
 import passport from "koa-passport";
+import { BaseRouter } from "./base/base-router";
 
-export class AuthRouter implements Router {
-  private readonly router: KoaRouter;
-
-  constructor() {
-    this.router = new KoaRouter();
-
-    this.setupRoutes();
-  }
-
-  get routes() {
-    return this.router.routes();
-  }
-
-  get allowedMethods() {
-    return this.router.allowedMethods();
-  }
-
-  private setupRoutes() {
+export class AuthRouter extends BaseRouter {
+  protected setupRoutes() {
     this.router.post(
       "/login",
       passport.authenticate("local", {
---
+++ src/api/routes/base/base-router.ts
@@ -0,0 +1,22 @@
+import { type Router } from "@/api/interfaces/router";
+import KoaRouter from "@koa/router";
+
+export abstract class BaseRouter implements Router {
+  protected readonly router: KoaRouter;
+
+  constructor() {
+    this.router = new KoaRouter();
+
+    this.setupRoutes();
+  }
+
+  get routes() {
+    return this.router.routes();
+  }
+
+  get allowedMethods() {
+    return this.router.allowedMethods();
+  }
+
+  protected abstract setupRoutes(): void;
+}
